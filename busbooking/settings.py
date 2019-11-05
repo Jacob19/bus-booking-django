a6572941f5938406c70e09a43faf6872737df80e
Jacob C F <jacob.c_f@nokia.com>
2019-11-05 11:53:25 +0530

    Change to access static files

--- busbooking/settings.py
+++ busbooking/settings.py
@@ -128,4 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'bookingapp/static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
