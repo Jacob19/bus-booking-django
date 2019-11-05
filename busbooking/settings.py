3658df369246f8993ee3a6320c1ea58b52bf466f
Jacob C F <jacob.c_f@nokia.com>
2019-11-05 12:56:31 +0530

    Adding static files and changing the setiings for the same, also pushing SQLite

--- busbooking/settings.py
+++ busbooking/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'bookingapp/static')
-STATIC_URL = '/static/'
+STATIC_URL = '/bookingapp/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bookingapp/static'),
 )
